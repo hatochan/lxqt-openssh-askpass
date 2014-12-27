edf2b61d8520e937c76ac6b000fba49993bcdeca
Kentaro Hatori <hatori@hatochan.dyndns.org>
2014-12-27 23:50:20 +0900

    Update and rename lxqt-openssh-askpass.ts to lxqt-openssh-askpass_ja.ts

--- translations/lxqt-openssh-askpass_ja.ts
+++ translations/lxqt-openssh-askpass_ja.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_US">
+<TS version="2.0" language="ja_JP">
 <context>
     <name>MainWindow</name>
     <message>
         <source>OpenSSH Authentication Passphrase request</source>
-        <translation type="unfinished"></translation>
+        <translation>OpenSSH認証パスフレーズのリクエスト</translation>
     </message>
     <message>
         <source>Enter your SSH passphrase for request:</source>
-        <translation type="unfinished"></translation>
+        <translation>リクエストに応えてSSHパスフレーズを入力してください:</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>unknown request</source>
-        <translation type="unfinished"></translation>
+        <translation>不明なリクエスト</translation>
     </message>
 </context>
 </TS>
